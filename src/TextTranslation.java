f7c327f526f41963e566c368fc08a0b3915ade19
bfontenot23 <beau_fontenot@icloud.com>
2024-04-05 13:51:06 -0500

    finished lab

--- src/TextTranslation.java
+++ src/TextTranslation.java
@@ -6,15 +6,14 @@ import java.util.Scanner;
 public class TextTranslation {
 
     static Scanner in = new Scanner(System.in);
-    public static Scanner getInputFile() throws FileNotFoundException
+    public static Scanner getInputFile(String fileName) throws FileNotFoundException
     {
         File file;
         boolean validFile = false;
         String filename, filepath;
         System.out.println();
         do {
-            System.out.print("\nEnter input filename: ");
-            filename = in.next();
+            filename = fileName;
             if(!filename.startsWith(":\\", 1) && !filename.startsWith(".\\") && !filename.startsWith(":/", 1) && !filename.startsWith("./") && !filename.startsWith("\\") && !filename.startsWith("/")) filepath = "./" + filename;
             else filepath = filename;
             file = new File(filepath);
@@ -38,13 +37,61 @@ public class TextTranslation {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<String, String>();
 
-// missing code for reading the file and loading the data into the map.
+        // missing code for reading the file and loading the data into the map.
+        Scanner input;
+        try
+        {
+            input = getInputFile("acronyms.txt");
+        }
+        catch (FileNotFoundException e)
+        {
+            return;
+        }
 
-        System.out.print("Enter abbreviated text: ");
-        String userInput = in.nextLine();
-        // Regular expression to split the string based on spaces
-        String[] words = userInput.split("\\s+");
-// missing code for hashmap lookup and string concatenation
+        boolean done = false;
+        do {
+            if(input.hasNextLine())
+            {
+                String line = input.nextLine();
+
+                String[] split = line.split(",");
+                map.put(split[0],split[1]);
+            }
+            else done = true;
+        }while(!done);
+
+        done = false;
+        String userInput;
+        String[] words;
+        StringBuilder out;
+        do {
+            System.out.print("Enter abbreviated text: ");
+            userInput = in.nextLine();
+
+            // Regular expression to split the string based on spaces
+            words = userInput.split("\\s+");
+
+            // missing code for hashmap lookup and string concatenation
+            out = new StringBuilder();
+            for(String word : words)
+            {
+                if(map.containsKey(word))
+                {
+                    out.append(map.get(word));
+                }
+                else out.append(word);
+                out.append(" ");
+            }
+
+            System.out.println(out.toString());
+
+            System.out.print("Would you like to continue?");
+            userInput = in.nextLine();
+
+            if(!userInput.toLowerCase().equals("yes")) done = true;
+
+        }while(!done);
+
 
         in.close();
     }
